ff1f0b8ec5b2ef7648c7ec6f24d763bdb9bd2604
lovely111 <=>
2016-04-08 10:44:55 +0800

    rename

--- src/H2.java
+++ src/H2.java
@@ -2,7 +2,7 @@
  * Created by Administrator on 2016/3/31.判断101-200之间有多少个素数，
  * 并输出所有素数。素数：只能被1和它本身整除的正整数（1不是素数）
  */
-public class PrimeNumber {
+public class H2 {
     public static void main(String[] args) {
         int count = 0;
         boolean b = false;
--- src/H3.java
+++ src/H3.java
@@ -2,7 +2,7 @@
  * Created by Administrator on 2016/3/31.打印出所有的“水仙花数”，所谓“水仙花数”是指一个三位数，
  * 其各位数字立方和等于该数本身。例如：153是一个“水仙花数’，因为153=1的三次方＋5的三次方＋3的三次方。
  */
-public class Flower {
+public class H3 {
    public static void main(String[] args) {
         for (int i=100;i<1000;i++){
             int a=i/100;
